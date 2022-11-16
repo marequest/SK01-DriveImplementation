ac73acc702d27be975075b82f01b1cd9a49028b4
Marko Jovicic <38961465+marequest@users.noreply.github.com>
2022-11-16 03:50:17 +0100

    Implemented download

--- src/main/java/com/sk01/driveimpl/DriveOperations.java
+++ src/main/java/com/sk01/driveimpl/DriveOperations.java
@@ -1,8 +1,11 @@
 package com.sk01.driveimpl;
 
+import com.google.api.services.drive.model.File;
 import com.sk01.storage.Operations;
+import com.sk01.utils.StorageInfo;
 
-import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 public class DriveOperations extends Operations {
@@ -17,22 +20,41 @@ public class DriveOperations extends Operations {
     }
 
     @Override
-    public void deleteDir(File dir) {
+    public void deleteDir(java.io.File dir) throws Exception {
 
     }
 
     @Override
-    public void moveFiles(List files, File dir1, File dir2) {
+    public void moveFiles(List files, java.io.File dirFrom, java.io.File dirTo) throws Exception {
+        for(Object file : files){
 
+            // TODO Mogli bi bez liste, sve fajlove iz dirFrom u dirTo?
+            // Ako ne, ne treba nam dirFrom jer to imam u file objektu
+
+        }
     }
 
     @Override
-    public void downloadFile(String pathFrom, String pathTo) {
+    public void downloadFile(String pathFrom, String pathTo) throws Exception{
+        pathFrom = StorageInfo.getInstance().getConfig().getPath() + pathFrom;
 
+        File driveFile = GoogleDrive.getFile(pathFrom);
+
+        java.io.File file = new java.io.File(pathTo);
+        OutputStream outputstream = new FileOutputStream(file);
+        GoogleDrive.getDriveService().files().get(driveFile.getId()).executeMediaAndDownloadTo(outputstream);
+        outputstream.flush();
+        outputstream.close();
     }
 
-    @Override
-    public void rename(File file) throws Exception {
-
-    }
+//    @Override
+//    public void rename(java.io.File file, String newName) throws Exception {
+//        File parent = GoogleDrive.getFile(file.getPath()); // TODO ne znam
+//        File fileMetadata = new File();
+//
+//        fileMetadata.setName(newName);
+//        fileMetadata.setParents(List.of(parent.getId()));
+//
+//        GoogleDrive.getDriveService().files().update(fileMetadata).setFields("id, name").execute();
+//    }
 }
