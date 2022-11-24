0da9faf6732ec537989df56c79f22400c91536ff
Ignjat Krdzavac <ikrdzavac11520rn@raf.rs>
2022-11-24 16:51:39 +0100

    commit

--- src/main/java/com/sk01/driveimpl/DriveSearch.java
+++ src/main/java/com/sk01/driveimpl/DriveSearch.java
@@ -18,8 +18,28 @@ public class DriveSearch extends Search {
     }
 
     @Override
+    public List<File> getFile(String path) throws Exception {
+        return null;
+    }
+
+    @Override
     public void getFiles(String extension) {
 
+    }
+
+    @Override
+    public List<File> getFilesWithExtension(String extension) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<File> getAllFiles(String path) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean containsFiles(String path, List fileNames) throws Exception {
+        return false;
     }
 
     @Override
@@ -45,6 +65,11 @@ public class DriveSearch extends Search {
     @Override
     public void sortByDate(List files) {
 
+    }
+
+    @Override
+    public List<File> getFiles(String path, Time pocetak, Time kraj) throws Exception {
+        return null;
     }
 
     @Override
